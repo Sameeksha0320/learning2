7a7bbfaabc14953baccf61b750564373164dea55
sameeksha0320 <sameeksha0320@gmail.com>
2025-08-08 18:27:09 +0530

    to-do list changed

--- src/App.js
+++ src/App.js
@@ -5,21 +5,23 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> To-do List</h1>
+        <h3>Dated : 08-08-2025</h3>
       </header>
+    <div className="sidebar"> 
+      <p> Things to be done today</p>
+      <ul>
+          <li>Hit the Gym </li>
+          <li>Go to College</li>
+          <li>Practice Coding</li>
+          <li>Practice dance</li>
+        </ul> </div>
+      <img src="logo111.jpg" alt="My Logo" className="corner-image" />
+
+
     </div>
   );
+
 }
 
 export default App;
